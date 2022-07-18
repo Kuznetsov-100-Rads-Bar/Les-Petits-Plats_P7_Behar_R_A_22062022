70f9c15b7922c5c632a074f3175472c11fb6ebb3
Kuznetsov-100-Rads-Bar <ledrifterdepripyat@gmail.com>
2022-07-18 12:42:39 +0200

    Ajout des commentaires

--- scripts/algorithms/algo.js
+++ scripts/algorithms/algo.js
@@ -8,16 +8,28 @@
 //     );
 // }
 
+/**
+ * Il prend un tableau de recettes et une chaîne d'entrée, et renvoie un tableau de recettes qui correspondent à la chaîne d'entrée
+ * @param recipes - an array of objects
+ * @param input - the search input
+ * @returns An array of objects.
+ */
 export const searchAlgo = (recipes, input) => {
+/* Création d'un tableau vide. */
     const list = [];
+    /* Boucler dans le tableau des recettes et vérifier si le nom ou la description correspond à l'entrée. */
     for (let recipe of recipes) {
         const recipeName = recipe.name.toLowerCase();
         const recipeDescription = recipe.description.toLowerCase();
 
+/* Création d'une expression régulière qui correspondra à la chaîne d'entrée. */
         const regex = new RegExp('(?:^|\\s)' + input.toLowerCase(), 'i');
+/* Tester si l'expression régulière correspond au nom de la recette. */
         const isNameMatch = regex.test(recipeName);
+/* Tester si l'expression régulière correspond à la description de la recette. */
         const isDescriptionMatch = regex.test(recipeDescription);
 
+        /* Boucler dans le tableau des ingrédients et vérifier si l'ingrédient correspond à l'entrée. */
         for (const ingredients of recipe.ingredients) {
             const isIngredientMatch = regex.test(ingredients.ingredient);
 
@@ -26,6 +38,7 @@ export const searchAlgo = (recipes, input) => {
             }
         }
 
+      /* Pousser la recette dans la liste si le nom ou la description correspond à l'entrée. */
         if (isNameMatch) {
             list.push(recipe);
         }
@@ -36,7 +49,9 @@ export const searchAlgo = (recipes, input) => {
     }
 
     // console.log(input)
+/* Suppression des doublons de la liste. */
     const filteredRecipes = [...new Set(list)];
 
+/* Renvoyer les recettes filtrées. */
     return filteredRecipes;
 }
--- scripts/components/searchBar.js
+++ scripts/components/searchBar.js
@@ -1,15 +1,36 @@
 // const errorMessage = document.getElementById("errorMessage");
+/* Importation de la fonction searchAlgo depuis le fichier algo.js. */
 import { searchAlgo } from "../algorithms/algo.js";
+/* Importation de l'instance cardsInstance depuis le fichier card.model.js. */
 import { cardsInstance } from "../models/card.model.js";
 
+/* Obtenir la valeur du champ de saisie. */
 const searchInput = document.getElementById('recipeSearch');
 
+/**
+ * Il prend la valeur de l'entrée de recherche et filtre les recettes en fonction de la valeur.
+ */
+/* Une fonction qui est appelée lorsque l'utilisateur tape dans le champ de recherche. */
 const searchInputHandler = () => {
+    /* Obtenir les recettes initiales des cartesInstance. */
     const initialRecipes = cardsInstance.getRecipes('initial');
+    /* Obtenir les recettes des cartesInstance. */
     const recipes = cardsInstance.getRecipes();
+/* Écoute d'un événement d'entrée sur l'élément searchInput. Lorsque l'événement est déclenché, il obtient le
+valeur de l'entrée et la transmet à la fonction searchAlgo. La fonction searchAlgo renvoie un
+tableau filtré de recettes. Le tableau filtré est ensuite passé à la méthode de mise à jour du
+Objet cardsInstance. La méthode update met à jour la propriété des recettes de l'objet cardsInstance.
+</code> */
     searchInput.addEventListener('input', async (event) => {
         const value = event.target.value;
 
+       /* Vérifier si la valeur n'est pas vide et si la longueur de la valeur est supérieure ou égale à 3.
+       Si c'est le cas, il appellera la fonction searchAlgo et passera les recettes et la valeur comme arguments.
+       La fonction searchAlgo renverra un tableau filtré de recettes. Le tableau filtré est alors
+       passé à la méthode de mise à jour de l'objet cardsInstance. La méthode de mise à jour met à jour le
+       Propriété de recettes de l'objet cardsInstance. Si la valeur est vide ou la longueur du
+       valeur est inférieure à 3, il mettra à jour la propriété recettes de l'objet cardsInstance avec
+       les recettes initiales. */
         if (value && value.length >= 3) {
             const filteredRecipes = await searchAlgo(recipes, value);
             if (filteredRecipes.length === 0) {
@@ -23,4 +44,5 @@ const searchInputHandler = () => {
     });
 }
 
+/* Exportation de la fonction searchInputHandler. */
 export { searchInputHandler };
