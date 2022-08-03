a3e6f9e5277295c078fda9dfd91aa3d5f740bf8c
Kuznetsov-100-Rads-Bar <ledrifterdepripyat@gmail.com>
2022-08-03 21:35:00 +0200

    mise à jour du fichier scss

--- scripts/models/ingredients.model.js
+++ scripts/models/ingredients.model.js
@@ -21,13 +21,20 @@ class Ingredients {
     this.init();
   }
 
+/* Une fonction qui est appelée lorsqu'un nouvel objet est créé. */
   init = () => {
     tagListInputIngredient.value = '';
 
+/* Ajout d'un écouteur d'événement à l'élément ingredientsListArrow. Lorsque l'élément est cliqué, le
+La fonction toggleIngredientsList est appelée. */
     ingredientListArrow.addEventListener('click', () => {
       this.toggleIngredientsList();
     });
 
+/* Écoute d'un événement d'entrée sur l'élément tagListInputIngredient. Si la valeur d'entrée est supérieure
+supérieur à 0, il appelle la fonction toggleIngredientsList avec un paramètre de 1. Si la valeur d'entrée est 0,
+il appelle la fonction toggleIngredientsList avec un paramètre de 0. Il appelle ensuite le
+fonction displayIngredientsList avec la valeur d'entrée comme paramètre. */
     tagListInputIngredient.addEventListener('input', (event) => {
       if (event.target.value.length > 0) {
         this.toggleIngredientsList(1);
@@ -37,6 +44,9 @@ class Ingredients {
       this.displayIngredientsList(event.target.value);
     });
 
+/* Écoute d'un événement de clic sur l'élément tagIngredientsList. Si la cible de l'événement a la classe
+tag-list-item, il appellera la fonction selectTag avec le textContent de la cible de l'événement comme
+paramètre. */
     tagIngredientsList.addEventListener('click', (event) => {
       if (event.target.classList.contains('tag-list-item')) {
         const item = event.target;
@@ -45,23 +55,33 @@ class Ingredients {
     })
   }
 
+/* Une fonction qui est appelée lorsqu'un nouvel objet est créé. */
+  /* Trier le tableau des ingrédients puis appeler la fonction displayIngredientsList(). */
   setupIngredientsList = (ingredients) => {
+/* Trier la gamme d'ingrédients. */
     this.ingredients = ingredients.sort();
     this.tempIngredients = ingredients.sort();
+    /* Appel de la fonction displayIngredientsList() et transmission de la valeur du champ de saisie. */
     this.displayIngredientsList();
+/* Retourne une chaîne. */
     return 'Setup ingredients list';
   }
 
+  /* Ajouter un ingrédient au tableau des ingrédients. */
   addIngredient = (ingredient) => {
+   /* puis trier le tableau, puis afficher le
+   liste des ingrédients. */
     this.ingredients.push(ingredient)
     this.ingredients = this.ingredients.sort();
     this.displayIngredientsList();
     return 'Ingredient added to list';
   }
 
+ /* Une fonction qui est appelée lorsqu'un nouvel objet est créé. */
   selectTag = (tag) => {
     const index = this.ingredients.findIndex((ingredient) => ingredient === tag);
 
+    /* Suppression de l'ingrédient sélectionné de la liste. */
     if (index >= 0) {
       this.ingredients.splice(index, 1);
       tagListInputIngredient.value = '';
@@ -75,14 +95,20 @@ class Ingredients {
     const containerClass = tagIngredientsContainer.classList;
     const isClassExists = Boolean(containerClass.contains('tag-open'));
 
+    /**
+     * Lorsque l'utilisateur clique sur le bouton 'ustensiles', le conteneur 'ustensiles' s'ouvre et le
+     * le conteneur 'appareils' se fermera. */
     const open = () => {
       containerClass.add('tag-open');
       tagUstensilsContainer.classList.remove('tag-open');
       tagAppliancesContainer.classList.remove('tag-open');
     }
+  
+   /* Il supprime la classe 'tag-open' de la variable containerClass */
     const close = () => {
       containerClass.remove('tag-open');
     }
+/* A ternary operator. */
     if (!status) {
       !isClassExists ? open()
         : close();
