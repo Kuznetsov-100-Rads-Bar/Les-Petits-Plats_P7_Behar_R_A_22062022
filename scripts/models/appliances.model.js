92f5196cf256e04578a91264ca30ac81d5bc9f52
Kuznetsov-100-Rads-Bar <ledrifterdepripyat@gmail.com>
2022-08-12 15:47:26 +0200

    Les derniers commentaires

--- scripts/models/appliances.model.js
+++ scripts/models/appliances.model.js
@@ -62,10 +62,13 @@ class Appliances {
     })
   }
 
-  /* A function that is called when the class is instantiated. */
+  /* Une fonction qui est appelée lorsque la classe est instanciée. */
+/* Trier le tableau des appliances puis appeler la fonction displayAppliancesList. */
   setupAppliancesList = (appliances) => {
+/* Trier la gamme d'appareils. */
     this.appliances = appliances.sort();
     this.tempAppliances = appliances.sort();
+/* Appel de la fonction displayAppliancesList. */
     this.displayAppliancesList();
     return 'Setup appliances list';
   }
@@ -78,8 +81,7 @@ class Appliances {
     return 'Appliance added to list';
   }
 
-  /* Fonction appelée lorsqu'une balise est sélectionnée. Il prend une balise en paramètre. Il trouve alors le
-  index de la balise dans le tableau appliances. Si l'indice est supérieur ou égal à 0, il supprime le
+  /* Fonction appelée lorsqu'une balise est sélectionnée. Il prend une balise en paramètre. Il trouve alors l'index de la balise dans le tableau appliances. Si l'indice est supérieur ou égal à 0, il supprime le
   balise du tableau des appareils, efface le champ de saisie, ajoute la balise à la tagCard et affiche le
   liste des appareils. Il renvoie ensuite une chaîne avec la balise sélectionnée. */
   selectTag = (tag) => {
@@ -94,15 +96,32 @@ class Appliances {
     return `Tag selectionné : ${tag}`;
   }
 
+/* A function that is called when the class is instantiated.
+Toggle the appliances list then call the displayAppliancesList function. */
   toggleAppliancesList = (status) => {
     const containerClass = tagAppliancesContainer.classList;
     const isClassExists = Boolean(containerClass.contains('tag-open'));
 
+   /**
+    * Lorsque l'utilisateur clique sur l'élément avec la classe tag-recipes, la fonction open() est appelée,
+    * qui ajoute la classe tag-open à l'élément avec la classe containerClass, et supprime la classe
+    * tag-open à partir des éléments avec les classes tag-ustensils et tag-ingredients.
+    */
     const open = () => {
+/* Ajout de la classe tag-open à la containerClass. */
       containerClass.add('tag-open');
+/* Suppression de la classe tag-open de l'élément avec la classe tag-ustensils. */
       tagUstensilsContainer.classList.remove('tag-open');
+/* Suppression de la classe tag-open de l'élément avec la classe tag-ingredients. */
       tagIngredientsContainer.classList.remove('tag-open');
     }
+   /**
+    * Il supprime la classe 'tag-open' de la variable containerClass.
+    */
+/**
+ * Si le statut n'est pas défini, alors vérifiez si la classe existe, si c'est le cas, alors supprimez-la, si ce n'est pas le cas, alors ajoutez-la. Si le statut est défini, alors vérifiez si c'est 1, si c'est le cas, alors ajoutez la classe,
+ * si c'est 0, alors supprimez la classe.
+ */
     const close = () => {
       containerClass.remove('tag-open');
     }
@@ -118,6 +137,12 @@ class Appliances {
     return 'Menu toggled';
   }
 
+/* Une fonction qui est appelée lorsque la classe est instanciée. Il prend un filtre en paramètre. Alors
+obtient la liste appliances et l'affecte à la variable appliancesList. Ensuite, il crée un nouveau tableau appliancesList  et l'affecte à la variable newAppliancesList. Il parcourt ensuite le
+appliancesList et obtient le nom de l'appliance et l'index du nom de l'appliance dans appliancesList
+déployer. Si l'indice est supérieur ou égal à 0, il supprime l'appareil de la appliancesList
+déployer. Il affecte ensuite le nouveau tableau appliancesList à la variable appliances. Il appelle alors le
+fonction displayAppliancesList. Il renvoie ensuite le tableau appliancesList. */
   displayAppliancesList = (filter) => {
     tagAppliancesList.innerHTML = '';
     const appliancesList = [...new Set(this.appliances)];
@@ -131,6 +156,9 @@ class Appliances {
       });
     } else {
       if (filter.length > 0) {
+      /* Checking if the appliancesList is not empty. If it is not empty, it will filter the
+      appliancesList and check if the filter is equal to the appliance. If it is equal, it will
+      create a new list item and append it to the tagAppliancesList. */
         if (appliancesList) {
           appliancesList.filter((appliance) => filter.toLowerCase() === appliance.toLowerCase()).forEach((appliance) => {
             const applianceItemList = document.createElement('li');
@@ -152,6 +180,13 @@ class Appliances {
     return this.appliances;
   };
 
+/*Une fonction qui est appelée lorsque la classe est instanciée. Il faut une gamme d'appareils comme un
+paramètre. Il obtient ensuite les balises de la tagCard et l'affecte aux balises variables. Alors
+crée un nouveau tableau d'appliances et l'affecte à la variable newAppliancesList. Il boucle alors
+à travers les balises et obtient le nom de la balise et l'index du nom de la balise dans le tableau d'appliances. Si
+l'indice est supérieur ou égal à 0, il supprime la balise du tableau d'appliances. Alors
+affecte le nouveau tableau d'appliances à la variable appliances. Il appelle alors la fonction
+displayAppliancesList. Il renvoie ensuite le tableau d'appliances. */
   updateAppliances = (appliances) => {
     const tags = tagCard.getTags();
     const newAppliancesList = appliances;
@@ -171,4 +206,5 @@ class Appliances {
   }
 }
 
+/* Exportation de la classe Appliances en tant que constante. */
 export const appliancesInstance = new Appliances();
